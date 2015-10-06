10d464730615903c6e1337586bca57f547faf7da
Gareth Down <g@garethdown.com>
2015-10-06 22:32:58 +0100

    correct config

--- src/script/system/getStreamingPrices/index.js
+++ src/script/system/getStreamingPrices/index.js
@@ -1,3 +1,14 @@
 const config = require('../../config');
 
-module.exports = require('./' + config.streamingPrices);
+// it seems you have to do it in this roundabout way to get
+// browserify to include the files
+//
+// doing:
+//   require('./' + config.streamingPrices);
+// does not work
+
+if (config.streamingPrices == 'server') {
+  module.exports = require('./server');
+} else if (config.streamingPrices == 'fake') {
+  module.exports = require('./fake');
+}
