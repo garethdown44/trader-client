2a248add10b651831b85c836ca13fa16b9da3853
Gareth Down <g@garethdown.com>
2016-04-03 17:55:13 +0100

    setup karma with webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,67 +1,36 @@
+var webpackConfig = require('./webpack.config')('development');
+webpackConfig.entry = {};
+
+var RewirePlugin = require("rewire-webpack");
+webpackConfig.plugins.push( new RewirePlugin() );
+
 module.exports = function(config) {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    files: [
+      'src/script/**/__tests__/*.js'
+    ],
 
     browsers: ['PhantomJS'],
 
-    phantomjsLauncher: {
-      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
-      //exitOnResourceError: true
-    },
+    frameworks: ['jasmine'],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'browserify'],
-
-    preprocessors: {
-      'src/script/**/__tests__/*.js': [ 'browserify' ],
-      //'src/**/*.js': ['babel']
-    },
-
-    browserify: {
-      debug: true,
-      transform: [ 'rewireify', 'babelify' ],
-      configure: function(bundle) {
-        bundle.on('prebundle', function() {
-          bundle.external('foobar');
-        });
-      }
-    },
-
-    // list of files / patterns to load in the browser
-    files: [
-        // tests location
-        'node_modules/babel-polyfill/dist/polyfill.js',
-        'src/script/**/__tests__/*.js',
-    ],
-
-
-    // list of files to exclude
-    exclude: [],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    preprocessors: {
+      'src/script/**/__tests__/*.js': [ 'webpack' ]
+    },
+
+    reporters: ['spec'],
+
     logLevel: config.LOG_INFO,
 
+    webpack: webpackConfig,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      // i. e.
+      noInfo: true
+    }
   });
 };
