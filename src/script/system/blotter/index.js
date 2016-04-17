0ae1d9174e55131ff9e2fef663828bdf843f9cb0
Gareth Down <g@garethdown.com>
2016-04-17 21:31:23 +0100

    remove dead code

--- src/script/system/blotter/index.js
+++ src/script/system/blotter/index.js
@@ -1,1 +1,13 @@
-module.exports = require('./server');
+const io = require('socket.io-client');
+const Rx = require('rx');
+const config = require('../../config');
+
+let socket = io.connect(config.serverUrl);
+
+const stream = Rx.Observable.create(obs => {
+  socket.on('position', function (position) {
+    obs.onNext(position);
+  });
+}).publish().refCount();
+
+module.exports = stream;
